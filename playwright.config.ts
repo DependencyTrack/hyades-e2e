804e81dd3c42c4040956143a75a97bae00d66dde
Thorben <169059276+elAlmani@users.noreply.github.com>
2025-07-18 17:44:13 +0200

    Prepare for PR to DTrack/e2e
    * update yaml
    
    Signed-off-by: Thorben Schmitt <thorben.schmitt@stud.h-da.de>
    
    * update playwright.config.ts and .gitignore
    
    Signed-off-by: Thorben Schmitt <thorben.schmitt@stud.h-da.de>
    
    * fix initial setup
    
    Signed-off-by: Thorben Schmitt <thorben.schmitt@stud.h-da.de>
    
    * prepare pr
    
    Signed-off-by: Thorben Schmitt <thorben.schmitt@stud.h-da.de>
    
    ---------
    
    Signed-off-by: Thorben Schmitt <thorben.schmitt@stud.h-da.de>

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
   timeout: 5 * 60 * 1000, // 5 min
   /* Set the timeout for each executed expect-line in ms */
   expect: {
-    timeout: 5 * 1000, // 5 sec
+    timeout: process.env.CI ? 10 * 1000 : 5 * 1000, // 10 sec : 5 sec
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
@@ -73,7 +73,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
 
     // Record trace only when retrying a test for the first time. 'off', 'on', 'retain-on-failure' and 'on-first-retry'
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
 
     // Record video only when retrying a test for the first time. 'off', 'on', 'retain-on-failure' and 'on-first-retry'
     video: {
@@ -83,9 +83,9 @@ export default defineConfig({
 
     /* low level timeouts in ms */
     // e.g. locator.click()
-    actionTimeout: 5 * 1000, // 5 sec
+    actionTimeout: process.env.CI ? 10 * 1000 : 5 * 1000, // 10 sec : 5 sec
     // e.g. page.goto()
-    navigationTimeout: 5 * 1000, // 5 sec
+    navigationTimeout: process.env.CI ? 10 * 1000 : 5 * 1000, // 10 sec : 5 sec
   },
 
   /* Configure projects for major browsers */
